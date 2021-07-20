8cc7c17239fccf3547284768fd2ae3b1ef8a99a6
Dale Stephenson <dalestephenson@zohomail.com>
2021-07-20 16:50:52 +1200

    Linkedin Learning Course Functions etc.

--- ll-course.sql
+++ ll-course.sql
@@ -1,3 +1,34 @@
 -- LinkedIn Learning Course
 -- Useful Functions
+-- Adventure Works
+
+-- String Functions
+
+SELECT FirstName
+    , LastName
+    , UPPER (FirstName) AS UpperCase
+    , LOWER (LastName) AS LowerCase
+    , LEN (FirstName) AS LengthOfFirstName
+    , LEFT (LastName, 3) AS FirstThreeLetters
+    , RIGHT (LastName, 3) AS LastThreeLetters
+    , TRIM (LastName) AS TrimmedName
+FROM Person.Person;
+
+-- Text Concatenation
+
+SELECT FirstName
+    , LastName
+    , CONCAT (FirstName, ' ', MiddleName, ' ', LastName) AS FullName
+    , CONCAT_WS (' ', FirstName, MiddleName, LastName) AS WithSeparators
+FROM Person.Person;
+
+-- Round with Mathematical Functions
+
+SELECT BusinessEntityID
+    , SalesYTD
+    , ROUND (SalesYTD, 2) AS Round2
+    , ROUND (SalesYTD, -2) AS RoundHundreds
+    , CEILING (SalesYTD) AS RoundCeiling
+    , FLOOR (SalesYTD) AS RoundFloor
+FROM Sales.SalesPerson;
 
