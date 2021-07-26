e1f29a3caf3c106b903557c5495b57c7a9bb996e
Dale Stephenson <dalestephenson@zohomail.com>
2021-07-26 15:46:04 +1200

    SQL Challenge 1

---
+++ challenge-1.sql
@@ -0,0 +1,44 @@
+-- SQL Challenge 1
+
+-- Example
+
+SELECT 
+    firstname,
+    lastname,
+    DepartmentName
+FROM DimEmployee
+WHERE DepartmentName = 'Marketing';
+
+-- Q. 1A
+
+SELECT 
+    firstname,
+    lastname,
+    DepartmentName,
+    StartDate,
+    `Status`
+FROM DimEmployee
+WHERE DepartmentName = 'Marketing' AND `Status` = 'Current';
+
+-- Q. 1B
+
+SELECT 
+    ProductKey,
+    EnglishProductName,
+    Color,
+    `Weight`
+FROM DimProduct
+WHERE `Weight` > 50
+ORBER BY `Weight`;
+
+-- Q. 1C
+
+SELECT  
+    SalesTerritoryKey,
+    SalesTerritoryRegion,
+    SalesTerritoryCountry,
+    SalesTerritoryGroup
+FROM DimSalesTerritory
+WHERE SalesTerritoryGroup = 'North America';
+
+
