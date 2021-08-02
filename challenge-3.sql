ea0b2ddad63565e1252166b2629856a1cb3a65a6
Dale Stephenson <dalestephenson@zohomail.com>
2021-08-02 15:52:06 +1200

    Add question 1

--- challenge-3.sql
+++ challenge-3.sql
@@ -5,3 +5,27 @@ ALTER AUTHORIZATION ON DATABASE:: [AdventureWorksDW2017] TO [sa]
 GO
 
 -- SQL Challenge 3
+
+-- Q. 1A
+-- Which 10 customers made the most orders during 2010? Return their name and
+-- the total number of orders they made during 2010.
+
+
+
+
+
+-- Q. 1B
+-- Google the union all statement and re-write this query so that you have the 
+-- top 3 selling products for Men and the top 3 selling products for Woman
+
+
+
+-- Q. 1C
+-- During 2011, there were two products where men are more than 4x as likely to 
+-- purchase these than women. Try to complete the following query:
+
+
+
+
+-- Q. 1D
+-- Try to rewrite this query without the CTE and without the LOG ratio.
