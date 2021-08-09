6df8d2b3e8b063e33ff05c91ade874f4f93f5846
Dale Stephenson <dalestephenson@zohomail.com>
2021-08-09 15:33:14 +1200

    over partition by clause

---
+++ over(partitionby).sql
@@ -0,0 +1,80 @@
+-- OVER(PARTITION BY) CLAUSE
+
+-- https://dotnettutorials.net/lesson/over-clause-sql-server/
+
+CREATE TABLE Employees
+(
+     ID INT,
+     Name VARCHAR(50),
+     Department VARCHAR(50),
+     Salary int
+)
+Go
+INSERT INTO Employees Values (1, 'James', 'IT', 15000)
+INSERT INTO Employees Values (2, 'Smith', 'IT', 35000)
+INSERT INTO Employees Values (3, 'Rasol', 'HR', 15000)
+INSERT INTO Employees Values (4, 'Rakesh', 'Payroll', 35000)
+INSERT INTO Employees Values (5, 'Pam', 'IT', 42000)
+INSERT INTO Employees Values (6, 'Stokes', 'HR', 15000)
+INSERT INTO Employees Values (7, 'Taylor', 'HR', 67000)
+INSERT INTO Employees Values (8, 'Preety', 'Payroll', 67000)
+INSERT INTO Employees Values (9, 'Priyanka', 'Payroll', 55000)
+INSERT INTO Employees Values (10, 'Anurag', 'Payroll', 15000)
+INSERT INTO Employees Values (11, 'Marshal', 'HR', 55000)
+INSERT INTO Employees Values (12, 'David', 'IT', 96000)
+
+SELECT  Department,
+    COUNT(*) AS NoOfEmployees,
+    SUM(Salary) AS TotalSalary,
+    AVG(Salary) AS AvgSalary,
+    MIN(Salary) AS MinSalary,
+    MAX(Salary) AS MaxSalary
+FROM Employees
+GROUP BY Department
+
+-- this fails
+
+SELECT  
+    Name, 
+    Salary, 
+    Department,
+    COUNT(*) AS NoOfEmployees,
+    SUM(Salary) AS TotalSalary,
+    AVG(Salary) AS AvgSalary,
+    MIN(Salary) AS MinSalary,
+    MAX(Salary) AS MaxSalary
+FROM Employees
+GROUP BY Department
+
+SELECT 
+    Name, 
+    Salary, 
+    Employees.Department,
+    Departments.DepartmentTotals,
+    Departments.TotalSalary,
+    Departments.AvgSalary,
+    Departments.MinSalary,
+    Departments.MaxSalary   
+FROM  Employees
+INNER JOIN
+(   SELECT 
+        Department, 
+        COUNT(*) AS DepartmentTotals,
+        SUM(Salary) AS TotalSalary,
+        AVG(Salary) AS AvgSalary,
+        MIN(Salary) AS MinSalary,
+        MAX(Salary) AS MaxSalary
+    FROM Employees
+    GROUP BY Department) AS Departments
+ON Departments.Department = Employees.Department
+    
+SELECT 
+    Name,
+    Salary,
+    Department,
+    COUNT(Department) OVER(PARTITION BY Department) AS DepartmentTotals,
+    SUM(Salary) OVER(PARTITION BY Department) AS TotalSalary,
+    AVG(Salary) OVER(PARTITION BY Department) AS AvgSalary,
+    MIN(Salary) OVER(PARTITION BY Department) AS MinSalary,
+    MAX(Salary) OVER(PARTITION BY Department) AS MaxSalary
+FROM Employees
