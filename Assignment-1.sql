6316c328fe8dade71705d8634cecba3982710a99
Dale Stephenson <dalestephenson@zohomail.com>
2021-08-15 21:05:54 +1200

    Q1

--- Assignment-1.sql
+++ Assignment-1.sql
@@ -20,11 +20,33 @@ GO
 
 -- 1A: (5 marks) Write & then run this query and include a screenshot of the results.
 
-select 
-        year(SalesOrderDate),
-        CountryName,
-        SegmentName,
-
+select
+    year(so.SalesOrderDate) as SalesYear,
+    CountryName,
+    SegmentName,
+    round(sum(SalePrice), 2) as TotalSale,
+    round(sum(SalePrice) - sum(ManufacturingPrice * UnitsSold), 2) as GrossProfit
+from SalesOrderLineItem li
+    inner join SalesOrder so
+    on li.SalesOrderID = so.SalesOrderID
+    inner join SalesRegion sr
+    on so.SalesRegionID = sr.SalesRegionID
+    inner join Region r
+    on sr.RegionID = r.RegionID
+    inner join Segment s
+    on r.SegmentID = s.SegmentID
+    inner join Country c
+    on r.CountryID = c.CountryID
+    inner join ProductCost pc
+    on c.CountryID = pc.CountryID
+group by
+    year(so.SalesOrderDate),
+    CountryName,
+    SegmentName
+order by
+    SalesYear,
+    CountryName,
+    SegmentName;
 
 -- 1B: (5 marks) Produce one or more visualisations using PowerBI to display this information.
 -- Based on your visualisations, which region performed the best? Which region performed the worst?
