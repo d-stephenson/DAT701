12a3b808e9ed43fb797debde0634069e56baeac2
Dale Stephenson <dalestephenson@zohomail.com>
2021-10-01 14:28:14 +1300

    Assignment 2 exec columns and field types

--- Assignment-2.sql
+++ Assignment-2.sql
@@ -1,1 +1,16 @@
 -- Assignment 2 | DAT701
+
+use FinanceDB
+go
+
+exec sp_columns Country
+exec sp_columns Product
+exec sp_columns ProductCost
+exec sp_columns Promotion
+exec sp_columns Region
+exec sp_columns SalesKPI
+exec sp_columns SalesOrder
+exec sp_columns SalesOrderLineItem
+exec sp_columns SalesPerson
+exec sp_columns SalesRegion
+exec sp_columns Segment
