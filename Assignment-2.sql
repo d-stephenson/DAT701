125c9a849e8a955f19c76a8de00c8b468b36a90f
Dale Stephenson <dalestephenson@zohomail.com>
2021-10-12 20:51:33 +1300

    insert into DimDate

--- Assignment-2.sql
+++ Assignment-2.sql
@@ -38,57 +38,142 @@ go
 create procedure create_tables
 as
 begin
+        
+    drop table if exists Dimdate;
+    drop table if exists DimProduct;
+    drop table if exists DimPromotion;
+    drop table if exists DimSalesLocation;
+    drop table if exists DimSalesPerson;
+    drop table if exists FactOrder;
+    drop table if exists FactSales;
+    drop table if exists FactAggregatedValues;
+
+    -- https://www.codeproject.com/Articles/647950/Create-and-Populate-date-Dimension-for-Data-Wareho
+
+    create table DimDate 
+    (
+        [dateKey] int primary key, 
+        [date] datetime,
+        [FulldateUK] char(10), -- date in dd-MM-yyyy format
+        [FulldateUSA] char(10),-- date in MM-dd-yyyy format
+        [DayOfMonth] varchar(2), -- Field will hold day number of Month
+        [DaySuffix] varchar(4), -- Apply suffix as 1st, 2nd ,3rd etc
+        [DayName] varchar(9), -- Contains name of the day, Sunday, Monday 
+        [DayOfWeekUSA] char(1),-- First Day Sunday=1 and Saturday=7
+        [DayOfWeekUK] char(1),-- First Day Monday=1 and Sunday=7
+        [DayOfWeekInMonth] varchar(2), --1st Monday or 2nd Monday in Month
+        [DayOfWeekInYear] varchar(2),
+        [DayOfQuarter] varchar(3),
+        [DayOfYear] varchar(3),
+        [WeekOfMonth] varchar(1),-- Week Number of Month 
+        [WeekOfQuarter] varchar(2), --Week Number of the Quarter
+        [WeekOfYear] varchar(2),--Week Number of the Year
+        [Month] varchar(2), --Number of the Month 1 to 12
+        [MonthName] varchar(9),--January, February etc
+        [MonthOfQuarter] varchar(2),-- Month Number belongs to Quarter
+        [Quarter] char(1),
+        [QuarterName] varchar(9),--First,Second..
+        [Year] char(4),-- Year value of date stored in Row
+        [YearName] char(7), --CY 2012,CY 2013
+        [MonthYear] char(10), --Jan-2013,Feb-2013
+        [MMYYYY] char(6),
+        [FirstDayOfMonth] date,
+        [LastDayOfMonth] date,
+        [FirstDayOfQuarter] date,
+        [LastDayOfQuarter] date,
+        [FirstDayOfYear] date,
+        [LastDayOfYear] date,
+        [IsHolidayUSA] bit,-- Flag 1=National Holiday, 0-No National Holiday
+        [IsWeekday] bit,-- 0=Week End ,1=Week Day
+        [HolidayUSA] varchar(50),--Name of Holiday in US
+        [IsHolidayUK] bit null,-- Flag 1=National Holiday, 0-No National Holiday
+        [HolidayUK] varchar(50) null --Name of Holiday in UK
+    );
     
-drop table if exists Dimdate;
-drop table if exists DimProduct;
-drop table if exists DimPromotion;
-drop table if exists DimSalesLocation;
-drop table if exists DiSalesPerson;
-drop table if exists FactOrder;
-drop table if exists FactSales;
-drop table if exists FactAggregatedValues;
+    create table DimProduct 
+    (
+        product_key tinyint identity,
+        ProductName varchar(24)
+    );
 
--- https://www.codeproject.com/Articles/647950/Create-and-Populate-date-Dimension-for-Data-Wareho
+    create table DimPromotion
+    (
+        promotion_key smallint identity,
+        PromotionYear int
+    );
 
-create table Dimdate (
-    [dateKey] int identity primary key, 
-    [date] datetime,
-    [FulldateUK] char(10), -- date in dd-MM-yyyy format
-    [FulldateUSA] char(10),-- date in MM-dd-yyyy format
-    [DayOfMonth] varchar(2), -- Field will hold day number of Month
-    [DaySuffix] varchar(4), -- Apply suffix as 1st, 2nd ,3rd etc
-    [DayName] varchar(9), -- Contains name of the day, Sunday, Monday 
-    [DayOfWeekUSA] char(1),-- First Day Sunday=1 and Saturday=7
-    [DayOfWeekUK] char(1),-- First Day Monday=1 and Sunday=7
-    [DayOfWeekInMonth] varchar(2), --1st Monday or 2nd Monday in Month
-    [DayOfWeekInYear] varchar(2),
-    [DayOfQuarter] varchar(3),
-    [DayOfYear] varchar(3),
-    [WeekOfMonth] varchar(1),-- Week Number of Month 
-    [WeekOfQuarter] varchar(2), --Week Number of the Quarter
-    [WeekOfYear] varchar(2),--Week Number of the Year
-    [Month] varchar(2), --Number of the Month 1 to 12
-    [MonthName] varchar(9),--January, February etc
-    [MonthOfQuarter] varchar(2),-- Month Number belongs to Quarter
-    [Quarter] char(1),
-    [QuarterName] varchar(9),--First,Second..
-    [Year] char(4),-- Year value of date stored in Row
-    [YearName] char(7), --CY 2012,CY 2013
-    [MonthYear] char(10), --Jan-2013,Feb-2013
-    [MMYYYY] char(6),
-    [FirstDayOfMonth] date,
-    [LastDayOfMonth] date,
-    [FirstDayOfQuarter] date,
-    [LastDayOfQuarter] date,
-    [FirstDayOfYear] date,
-    [LastDayOfYear] date,
-    [IsHolidayUSA] bit,-- Flag 1=National Holiday, 0-No National Holiday
-    [IsWeekday] bit,-- 0=Week End ,1=Week Day
-    [HolidayUSA] varchar(50),--Name of Holiday in US
-    [IsHolidayUK] bit null,-- Flag 1=National Holiday, 0-No National Holiday
-    [HolidayUK] varchar(50) null --Name of Holiday in UK
-);
-go 
+    create table DimSalesLocation
+    (
+        saleslocation_key smallint identity,
+        CountryName varchar(56),
+        SegmentName varchar(48)
+    );
+
+    create table DimSalesPerson
+    (
+        salesperson_key smallint identity,
+        FirstName varchar(64),
+        LastName varchar(64),
+        Gender
+        HireDate
+        DateOfBirth
+        DateOfLeave
+        DateOfSickLeave
+    );
+
+
+
+
+
+    drop table if exists Di;
+    drop table if exists FactOrder;
+    drop table if exists FactSales;
+    drop table if exists FactAggregatedValues;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
@@ -172,7 +257,6 @@ BEGIN
        
         /* Check for Change in Year of the Current date if Year changed then change 
          Variable value*/
-	
 
 	IF @CurrentYear != DATEPART(YY, @CurrentDate)
 	BEGIN
@@ -198,7 +282,6 @@ BEGIN
 	WHERE DOW = DATEPART(DW, @CurrentDate)
 	
 /*End day of week logic*/
-
 
 /* Populate Your Dimension Table with values*/
 	
