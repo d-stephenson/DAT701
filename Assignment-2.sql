cfe85a4b081bb82d7af284c75ab71648dd3f64d1
Dale Stephenson <dalestephenson@zohomail.com>
2021-10-12 20:38:12 +1300

    create dim_date table

--- Assignment-2.sql
+++ Assignment-2.sql
@@ -39,52 +39,53 @@ create procedure create_tables
 as
 begin
     
-drop table if exists dim_date;
-drop table if exists dim_product;
-drop table if exists dim_promotion;
-drop table if exists dim_sales_location;
-drop table if exists dim_sales_person;
-drop table if exists fact_order;
-drop table if exists fact_sales;
-drop table if exists fact_aggregated_values;
+drop table if exists Dimdate;
+drop table if exists DimProduct;
+drop table if exists DimPromotion;
+drop table if exists DimSalesLocation;
+drop table if exists DiSalesPerson;
+drop table if exists FactOrder;
+drop table if exists FactSales;
+drop table if exists FactAggregatedValues;
 
-create table dim_date (
-    [DateKey] int identity primary key, 
-    [Date] DATETIME,
-    [FullDateUK] CHAR(10), -- Date in dd-MM-yyyy format
-    [FullDateUSA] CHAR(10),-- Date in MM-dd-yyyy format
-    [DayOfMonth] VARCHAR(2), -- Field will hold day number of Month
-    [DaySuffix] VARCHAR(4), -- Apply suffix as 1st, 2nd ,3rd etc
-    [DayName] VARCHAR(9), -- Contains name of the day, Sunday, Monday 
-    [DayOfWeekUSA] CHAR(1),-- First Day Sunday=1 and Saturday=7
-    [DayOfWeekUK] CHAR(1),-- First Day Monday=1 and Sunday=7
-    [DayOfWeekInMonth] VARCHAR(2), --1st Monday or 2nd Monday in Month
-    [DayOfWeekInYear] VARCHAR(2),
-    [DayOfQuarter] VARCHAR(3),
-    [DayOfYear] VARCHAR(3),
-    [WeekOfMonth] VARCHAR(1),-- Week Number of Month 
-    [WeekOfQuarter] VARCHAR(2), --Week Number of the Quarter
-    [WeekOfYear] VARCHAR(2),--Week Number of the Year
-    [Month] VARCHAR(2), --Number of the Month 1 to 12
-    [MonthName] VARCHAR(9),--January, February etc
-    [MonthOfQuarter] VARCHAR(2),-- Month Number belongs to Quarter
-    [Quarter] CHAR(1),
-    [QuarterName] VARCHAR(9),--First,Second..
-    [Year] CHAR(4),-- Year value of Date stored in Row
-    [YearName] CHAR(7), --CY 2012,CY 2013
-    [MonthYear] CHAR(10), --Jan-2013,Feb-2013
-    [MMYYYY] CHAR(6),
-    [FirstDayOfMonth] DATE,
-    [LastDayOfMonth] DATE,
-    [FirstDayOfQuarter] DATE,
-    [LastDayOfQuarter] DATE,
-    [FirstDayOfYear] DATE,
-    [LastDayOfYear] DATE,
-    [IsHolidayUSA] BIT,-- Flag 1=National Holiday, 0-No National Holiday
-    [IsWeekday] BIT,-- 0=Week End ,1=Week Day
-    [HolidayUSA] VARCHAR(50),--Name of Holiday in US
-    [IsHolidayUK] BIT Null,-- Flag 1=National Holiday, 0-No National Holiday
-    [HolidayUK] VARCHAR(50) Null --Name of Holiday in UK
+-- https://www.codeproject.com/Articles/647950/Create-and-Populate-date-Dimension-for-Data-Wareho
+
+create table Dimdate (
+    [dateKey] int identity primary key, 
+    [date] datetime,
+    [FulldateUK] char(10), -- date in dd-MM-yyyy format
+    [FulldateUSA] char(10),-- date in MM-dd-yyyy format
+    [DayOfMonth] varchar(2), -- Field will hold day number of Month
+    [DaySuffix] varchar(4), -- Apply suffix as 1st, 2nd ,3rd etc
+    [DayName] varchar(9), -- Contains name of the day, Sunday, Monday 
+    [DayOfWeekUSA] char(1),-- First Day Sunday=1 and Saturday=7
+    [DayOfWeekUK] char(1),-- First Day Monday=1 and Sunday=7
+    [DayOfWeekInMonth] varchar(2), --1st Monday or 2nd Monday in Month
+    [DayOfWeekInYear] varchar(2),
+    [DayOfQuarter] varchar(3),
+    [DayOfYear] varchar(3),
+    [WeekOfMonth] varchar(1),-- Week Number of Month 
+    [WeekOfQuarter] varchar(2), --Week Number of the Quarter
+    [WeekOfYear] varchar(2),--Week Number of the Year
+    [Month] varchar(2), --Number of the Month 1 to 12
+    [MonthName] varchar(9),--January, February etc
+    [MonthOfQuarter] varchar(2),-- Month Number belongs to Quarter
+    [Quarter] char(1),
+    [QuarterName] varchar(9),--First,Second..
+    [Year] char(4),-- Year value of date stored in Row
+    [YearName] char(7), --CY 2012,CY 2013
+    [MonthYear] char(10), --Jan-2013,Feb-2013
+    [MMYYYY] char(6),
+    [FirstDayOfMonth] date,
+    [LastDayOfMonth] date,
+    [FirstDayOfQuarter] date,
+    [LastDayOfQuarter] date,
+    [FirstDayOfYear] date,
+    [LastDayOfYear] date,
+    [IsHolidayUSA] bit,-- Flag 1=National Holiday, 0-No National Holiday
+    [IsWeekday] bit,-- 0=Week End ,1=Week Day
+    [HolidayUSA] varchar(50),--Name of Holiday in US
+    [IsHolidayUK] bit null,-- Flag 1=National Holiday, 0-No National Holiday
+    [HolidayUK] varchar(50) null --Name of Holiday in UK
 );
-
 go 
