8fe0110443d86d3bc6ef6ec70c167fed985704b0
Dale Stephenson <dalestephenson@zohomail.com>
2021-11-15 20:22:37 +1300

    minor updates

--- Assignment-2.sql
+++ Assignment-2.sql
@@ -1,20 +1,17 @@
 -- DAT602 | Assignment 2
 -- Data Warehouse
 
-use FinanceDB
-go
-
-exec sp_columns Country
-exec sp_columns Product
-exec sp_columns ProductCost
-exec sp_columns Promotion
-exec sp_columns Region
-exec sp_columns SalesKPI
-exec sp_columns SalesOrder
-exec sp_columns SalesOrderLineItem
-exec sp_columns SalesPerson
-exec sp_columns SalesRegion
-exec sp_columns Segment
+exec sp_columns Country;
+exec sp_columns Product;
+exec sp_columns ProductCost;
+exec sp_columns Promotion;
+exec sp_columns Region;
+exec sp_columns SalesKPI;
+exec sp_columns SalesOrder;
+exec sp_columns SalesOrderLineItem;
+exec sp_columns SalesPerson;
+exec sp_columns SalesRegion;
+exec sp_columns Segment;
 
 -- >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
 
